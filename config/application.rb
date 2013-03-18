8f6bd16db87ca59e9b598b5cf13b31aad685bcfa
mangala <mangala.mangala176@example.com>
2013-03-18 15:10:17 +0530

    fixing gecoder issue

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require 'rails/all'
 require 'fb_graph'
 require 'devise'
 require 'paperclip'
+require 'geocoder'
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
